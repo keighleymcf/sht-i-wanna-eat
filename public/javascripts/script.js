708164647cc88d1d551e9037b822c541a6ca5940
Noriko Takizawa <norikotakizawa@Norikos-MacBook-Pro.local>
2019-09-19 10:30:44 +0200

    change nav bar

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,18 +7,33 @@ document.addEventListener(
   false
 );
 
-const navIcon = document.querySelector('#nav-icon');
-navIcon.onclick = () => {
-  navIcon.classList.toggle('open');
+const tabmenu = () => {
+  'use strict';
+  const navIcon = document.querySelector('#nav-icon');
+
+  navIcon.addEventListener('click', () => {
+
+    document.body.classList.toggle('background--blur');
+    navIcon.classList.toggle('open')
+    const menu = document.querySelector(".menu--off ")
+    menu.classList.toggle('menu--on');
+
+  });
 }
+tabmenu();
 
 const showHide = () => {
+  console.log("SH")
   let miniWindow = document.querySelector('.miniMenu')
-  if (miniWindow)
+  if (miniWindow.style.display === "none") {
+    miniWindow.style.display = "block";
+  } else {
+    miniWindow.style.display = "none";
+  }
 }
-const popUp = document.querySelector(".popUp").onclick = () => {
-  document.querySelector('.miniMenu').display = 'none';
-}
+const Show = document.querySelector(".popUp").onclick = showHide;
+const hide = document.querySelector(".hideUp").onclick = showHide;
+
 
 
 
